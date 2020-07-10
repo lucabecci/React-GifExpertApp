55265a8c189109b89a2138c6eaa7c3acc8d97ca0
lukaBRI <beccibrian@gmail.com>
2020-07-10 05:08:13 -0300

    Docs agregado

---
+++ docs/precache-manifest.ab5157638ddbbf72dd58e813dd47027f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba38e80bcf39659351030bd8b9f331a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6102e8f3fb8d79d0d344",
+    "url": "/static/css/main.e44df14d.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6102e8f3fb8d79d0d344",
+    "url": "/static/js/main.31ec6575.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
